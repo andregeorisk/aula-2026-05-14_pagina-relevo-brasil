f1201ce3d142da8ae2e23689cc1ceae9f41deb9a
André Silva <andregeorisk@gmail.com>
2026-05-14 11:01:26 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+// Função chamada quando o usuário clica no botão 1
+function alterarTexto1() {
+
+    // Localiza o elemento com id="texto"
+    let paragrafo = document.getElementById("texto");
+
+    // Altera o conteúdo do parágrafo
+    paragrafo.innerHTML = "Viu, o JavaScript alterou este texto!";
+}
+
+function alterarTexto2() {
+
+    // Localiza o elemento com id="texto"
+    let paragrafo = document.getElementById("texto");
+
+    // Altera o conteúdo do parágrafo
+    paragrafo.innerHTML = "Esse texto vai mudar se você clicar no botão...";
+}
